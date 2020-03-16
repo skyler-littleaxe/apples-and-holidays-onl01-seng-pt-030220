1bac694ae10128af94f4af466c8c88d50ea5244a
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-16 03:12:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday_objects|
     puts season.to_s.capitalize! + ":"
-    holiday_object.each do |holiday, supply_array|
+    holiday_objects.each do |holiday, supply_array|
       holiday_name_array = holiday.to_s.split("_")
       holiday_name_array_capitalized = holiday_name_array.collect do |name|
         name.capitalize!
