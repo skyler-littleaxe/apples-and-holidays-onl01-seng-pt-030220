c22f57a160d448f28b002184cb48de7bdb7c4cfc
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-16 02:38:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,6 +46,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  winter_supplies = []
+  holiday_hash[:winter]
 
 end
 
